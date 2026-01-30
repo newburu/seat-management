b32c5043cb95e98bd24420ffb10014e16985935a
Newburu <newburu@gmail.com>
2026-01-31 00:35:39 +0900

    feat: 参加者一覧テーブルにレスポンシブデザインを適用し、モバイル表示を改善

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   has_many :participants, dependent: :destroy
   has_many :groupings, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 50 }
 end
