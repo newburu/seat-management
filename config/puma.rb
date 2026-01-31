d34bb8552605304bd3d67ed9350a618b4b2e0f21
Newburu <newburu@gmail.com>
2026-01-31 21:53:50 +0900

    refactor: 開発環境のPuma設定でポートとPIDファイルを明示的に指定するように変更

--- config/puma.rb
+++ config/puma.rb
@@ -27,18 +27,13 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Port is configured in the conditional block below)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 # Production specific configuration
 # Check for production environment or presence of deployment directory
@@ -62,4 +57,8 @@ if ENV.fetch("RAILS_ENV", "development") == "production" || File.exist?("/var/ww
 
   # Prune bundler
   prune_bundler
+else
+  # Development specific configuration
+  port ENV.fetch("PORT") { 3000 }
+  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 end
